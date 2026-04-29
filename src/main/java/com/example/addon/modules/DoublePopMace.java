3db7cf32c4ce9c0e45932cfbe9ba22fdce4d91d6
pgbt <hungpham0127@gmail.com>
2026-04-29 21:52:05 +0700

    Update DoublePopMace.java

--- src/main/java/com/example/addon/modules/DoublePopMace.java
+++ src/main/java/com/example/addon/modules/DoublePopMace.java
@@ -1,4 +1,4 @@
-package your.package.name.modules;
+package com.example.addon.modules;
 
 import meteordevelopment.meteorclient.events.packets.PacketEvent;
 import meteordevelopment.meteorclient.systems.modules.Category;
@@ -7,23 +7,20 @@ import meteordevelopment.orbit.EventHandler;
 import net.minecraft.network.packet.c2s.play.PlayerInteractEntityC2SPacket;
 import net.minecraft.entity.Entity;
 
-public class MultiAttack extends Module {
-    public MultiAttack() {
-        // You can change Category.COMBAT to any existing category
-        super(Category.COMBAT, "multi-attack", "Sends an extra attack packet on every hit.");
+public class DoublePopMace extends Module {
+    public DoublePopMace() {
+        // Using Category.COMBAT by default
+        super(Category.COMBAT, "double-pop-mace", "Sends an extra attack packet in the same tick.");
     }
 
     @EventHandler
     private void onSendPacket(PacketEvent.Send event) {
-        // Check if the outgoing packet is an attack packet
         if (event.packet instanceof PlayerInteractEntityC2SPacket packet) {
-            
-            // Extract the entity being attacked using an accessor or reflections if needed
-            // For standard fabric/meteor development, accessors are usually available
+            // Accessing the entity from the packet
             Entity entity = packet.getEntity(mc.world);
             
             if (entity != null) {
-                // Send an identical attack packet immediately
+                // Instantly send the second attack packet
                 mc.player.networkHandler.sendPacket(PlayerInteractEntityC2SPacket.attack(entity, mc.player.isSneaking()));
             }
         }
