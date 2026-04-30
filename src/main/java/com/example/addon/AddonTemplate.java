a3f63529f4dda6d2b3f676dfb709bcb14bb88c40
pgbt <hungpham0127@gmail.com>
2026-04-30 13:23:29 +0700

    Update AddonTemplate.java

--- src/main/java/com/example/addon/AddonTemplate.java
+++ src/main/java/com/example/addon/AddonTemplate.java
@@ -1,25 +1,27 @@
 package com.example.addon;
 
-import com.example.addon.modules.DoubleAttack;
+import com.example.addon.modules.DoubleAttack; // Import your module
+import com.mojang.logging.LogUtils;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
-import meteordevelopment.meteorclient.systems.hud.HudGroup;
-import meteordevelopment.meteorclient.systems.modules.Category;
-import meteordevelopment.meteorclient.systems.modules.Categories;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-public class AddonTemplate extends MeteorAddon {
-    public static final Logger LOG = LoggerFactory.getLogger("Double Pop Addon");
-    public static final Category CATEGORY = Categories.Combat;
-    public static final HudGroup HUD_GROUP = new HudGroup("DoublePop");
+public class Addon extends MeteorAddon {
+    public static final Logger LOG = LogUtils.getLogger();
 
     @Override
     public void onInitialize() {
-        LOG.info("Initializing Double Attack...");
+        LOG.info("Initializing DoubleAttack Addon...");
 
-        // Register the module
+        // Modules
+        // This line registers your DoubleAttack module so it appears in the ClickGUI
         Modules.get().add(new DoubleAttack());
+    }
+
+    @Override
+    public void onRegisterCategories() {
+        // You can register custom categories here if needed
+        // For now, we are using the default 'Combat' category
     }
 
     @Override
