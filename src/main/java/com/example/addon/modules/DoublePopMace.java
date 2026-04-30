311ceeb195bccd811c6e1acdd3b758aeffcada51
pgbt <hungpham0127@gmail.com>
2026-04-30 07:48:52 +0700

    Update DoublePopMace.java

--- src/main/java/com/example/addon/modules/DoublePopMace.java
+++ src/main/java/com/example/addon/modules/DoublePopMace.java
@@ -1,27 +1,28 @@
 package com.example.addon.modules;
 
 import meteordevelopment.meteorclient.events.packets.PacketEvent;
-import meteordevelopment.meteorclient.systems.modules.Category;
+import meteordevelopment.meteorclient.systems.modules.Categories;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.orbit.EventHandler;
-import net.minecraft.network.packet.c2s.play.PlayerInteractEntityC2SPacket;
-import net.minecraft.entity.Entity;
+import net.minecraft.network.protocol.game.ServerboundInteractPacket;
+import net.minecraft.world.entity.Entity;
 
 public class DoublePopMace extends Module {
     public DoublePopMace() {
-        // Using Category.COMBAT by default
-        super(Category.COMBAT, "double-pop-mace", "Sends an extra attack packet in the same tick.");
+        // Changed Category.COMBAT to Categories.Combat to match Meteor's Mojang mapping style
+        super(Categories.Combat, "double-pop-mace", "Sends an extra attack packet in the same tick.");
     }
 
     @EventHandler
     private void onSendPacket(PacketEvent.Send event) {
-        if (event.packet instanceof PlayerInteractEntityC2SPacket packet) {
-            // Accessing the entity from the packet
-            Entity entity = packet.getEntity(mc.world);
+        if (event.packet instanceof ServerboundInteractPacket packet) {
+            // In Mojang mappings, we use a specific way to get the entity
+            // Using Meteor's 'mc' instance which is available in the Module class
+            Entity entity = packet.getTarget(mc.level);
             
             if (entity != null) {
-                // Instantly send the second attack packet
-                mc.player.networkHandler.sendPacket(PlayerInteractEntityC2SPacket.attack(entity, mc.player.isSneaking()));
+                // Send the extra attack packet
+                mc.getConnection().send(ServerboundInteractPacket.createAttackPacket(entity, mc.player.isShiftKeyDown()));
             }
         }
     }
