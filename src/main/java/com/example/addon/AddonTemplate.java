78ffbcf6fca41d7b0692829f9162a3f3e511d388
pgbt <hungpham0127@gmail.com>
2026-04-30 08:15:16 +0700

    Update AddonTemplate.java

--- src/main/java/com/example/addon/AddonTemplate.java
+++ src/main/java/com/example/addon/AddonTemplate.java
@@ -1,15 +1,15 @@
 package com.example.addon;
 
 import com.example.addon.modules.DoubleAttack;
-import meteordevelopment.meteorclient.addons.GithubRepo;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
 import meteordevelopment.meteorclient.systems.hud.HudGroup;
 import meteordevelopment.meteorclient.systems.modules.Category;
+import meteordevelopment.meteorclient.systems.modules.Categories;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Addon extends MeteorAddon {
+public class AddonTemplate extends MeteorAddon {
     public static final Logger LOG = LoggerFactory.getLogger("Double Pop Addon");
     public static final Category CATEGORY = Categories.Combat;
     public static final HudGroup HUD_GROUP = new HudGroup("DoublePop");
@@ -18,13 +18,8 @@ public class Addon extends MeteorAddon {
     public void onInitialize() {
         LOG.info("Initializing Double Attack...");
 
-        // Register your module here
+        // Register the module
         Modules.get().add(new DoubleAttack());
-    }
-
-    @Override
-    public void onRegisterCategories() {
-        // Categories are registered automatically if using standard ones
     }
 
     @Override
