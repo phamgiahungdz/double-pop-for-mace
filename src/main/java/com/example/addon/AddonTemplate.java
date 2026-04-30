893c2752a35150bc2c513f09db7bc5cdbbcfe13c
pgbt <hungpham0127@gmail.com>
2026-04-30 07:48:37 +0700

    Update AddonTemplate.java

--- src/main/java/com/example/addon/AddonTemplate.java
+++ src/main/java/com/example/addon/AddonTemplate.java
@@ -3,6 +3,7 @@ package com.example.addon;
 import com.example.addon.modules.DoublePopMace;
 import com.mojang.logging.LogUtils;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
+import meteordevelopment.meteorclient.systems.hud.HudGroup;
 import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import org.slf4j.Logger;
@@ -10,6 +11,9 @@ import org.slf4j.Logger;
 public class AddonTemplate extends MeteorAddon {
     public static final Logger LOG = LogUtils.getLogger();
     public static final Category CATEGORY = new Category("Mace Addon");
+    
+    // Define the HUD_GROUP that HudExample.java is looking for
+    public static final HudGroup HUD_GROUP = new HudGroup("Mace HUD");
 
     @Override
     public void onInitialize() {
