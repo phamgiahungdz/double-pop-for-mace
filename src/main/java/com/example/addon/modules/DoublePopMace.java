7d85b3314761c45a0695089e65f4dfb8654125d2
pgbt <hungpham0127@gmail.com>
2026-04-30 08:02:05 +0700

    Update DoublePopMace.java

--- src/main/java/com/example/addon/modules/DoublePopMace.java
+++ src/main/java/com/example/addon/modules/DoublePopMace.java
@@ -1,35 +1,36 @@
 package com.example.addon.modules;
 
 import meteordevelopment.meteorclient.events.packets.PacketEvent;
-import meteordevelopment.meteorclient.systems.modules.Category;
+import meteordevelopment.meteorclient.systems.modules.Categories;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.orbit.EventHandler;
-import net.minecraft.network.packet.c2s.play.PlayerInteractEntityC2SPacket;
-import net.minecraft.util.hit.EntityHitResult;
-import net.minecraft.util.hit.HitResult;
-import net.minecraft.entity.Entity;
+import net.minecraft.network.protocol.game.ServerboundInteractPacket;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.phys.EntityHitResult;
+import net.minecraft.world.phys.HitResult;
 
 public class DoublePopMace extends Module {
     public DoublePopMace() {
-        // Using Category.COMBAT (Yarn name)
-        super(Category.COMBAT, "double-pop-mace", "Sends an extra attack packet in the same tick.");
+        // Categories.Combat is the correct 1.21 Mojang name
+        super(Categories.Combat, "double-pop-mace", "Sends an extra attack packet in the same tick.");
     }
 
     @EventHandler
     private void onSendPacket(PacketEvent.Send event) {
-        // PlayerInteractEntityC2SPacket is the Yarn name for the attack packet
-        if (event.packet instanceof PlayerInteractEntityC2SPacket packet) {
+        // ServerboundInteractPacket is the Mojang name for attack/interact
+        if (event.packet instanceof ServerboundInteractPacket packet) {
             
-            // In 1.21 Yarn, mc.crosshairTarget is usually mc.crosshairTarget 
-            // but we must check if it's an Entity hit
-            if (mc.crosshairTarget != null && mc.crosshairTarget.getType() == HitResult.Type.ENTITY) {
-                Entity entity = ((EntityHitResult) mc.crosshairTarget).getEntity();
+            // In modern versions, 'crosshairTarget' is renamed to 'hitResult'
+            if (mc.hitResult != null && mc.hitResult.getType() == HitResult.Type.ENTITY) {
+                Entity entity = ((EntityHitResult) mc.hitResult).getEntity();
 
                 if (entity != null) {
-                    // This creates a second attack packet for the same entity
-                    mc.player.networkHandler.sendPacket(PlayerInteractEntityC2SPacket.attack(
+                    // Logic to send the second attack packet
+                    // We use performInteraction to create the attack packet
+                    mc.getConnection().send(ServerboundInteractPacket.performInteraction(
                         entity, 
-                        mc.player.isSneaking()
+                        mc.player.isShiftKeyDown(), 
+                        ServerboundInteractPacket.InteractionAction.ATTACK
                     ));
                 }
             }
