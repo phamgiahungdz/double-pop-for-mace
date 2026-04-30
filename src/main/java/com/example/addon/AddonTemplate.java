b129b11057120b2fec9e95550ad9035b3d894768
pgbt <hungpham0127@gmail.com>
2026-04-30 08:11:56 +0700

    Update AddonTemplate.java

--- src/main/java/com/example/addon/AddonTemplate.java
+++ src/main/java/com/example/addon/AddonTemplate.java
@@ -1,25 +1,30 @@
 package com.example.addon;
 
-import com.example.addon.modules.DoubleAttack; // Import your module class
+import com.example.addon.modules.DoubleAttack;
+import meteordevelopment.meteorclient.addons.GithubRepo;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
+import meteordevelopment.meteorclient.systems.hud.HudGroup;
+import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Addon extends MeteorAddon {
-    public static final Logger LOG = LoggerFactory.getLogger("Example Addon");
+    public static final Logger LOG = LoggerFactory.getLogger("Double Pop Addon");
+    public static final Category CATEGORY = Categories.Combat;
+    public static final HudGroup HUD_GROUP = new HudGroup("DoublePop");
 
     @Override
     public void onInitialize() {
-        LOG.info("Initializing Double Attack Addon...");
+        LOG.info("Initializing Double Attack...");
 
-        // This line registers your custom module into the Meteor Client system
+        // Register your module here
         Modules.get().add(new DoubleAttack());
     }
 
     @Override
     public void onRegisterCategories() {
-        // Use this if you want to create a custom category instead of using Combat
+        // Categories are registered automatically if using standard ones
     }
 
     @Override
